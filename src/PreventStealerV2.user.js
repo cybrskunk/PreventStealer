718aecf06a22532c6610c7db1df6a86ed3426582
scape.goat <143356794+cybrskunk@users.noreply.github.com>
2025-08-04 12:30:27 +0200

    document-start

--- src/PreventStealerV2.user.js
+++ src/PreventStealerV2.user.js
@@ -7,6 +7,7 @@
 // @match        https://www.kogama.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kogama.com
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 // Now also blocks requests trying to access password-endpoints
